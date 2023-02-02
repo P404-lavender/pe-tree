75b02e33834ca8434f585ff76d3bb7b6da57a7d1
xenxynon <flynryder427@gmail.com>
2023-02-02 18:45:15 +0530

    Revert "Revert "sdm660-common: use nexus clang""
    This reverts commit 98c65762281709119a00f8619e7d17f2b8e6fd5e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,12 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+KERNEL_LLVM_SUPPORT := true
+KERNEL_CUSTOM_LLVM := true
+KERNEL_CUSTOM_LLVM_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-nexus
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX_ARM32 := arm-linux-gnueabi-
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
