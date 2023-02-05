1df7096eb1175b119d13334b5f91ffa12f0de803
xenxynon <flynryder427@gmail.com>
2023-02-05 13:29:22 +0530

    missing separator

--- sdm660.mk
+++ sdm660.mk
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@1.1 \
     vendor.display.config@1.9 \
     vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
